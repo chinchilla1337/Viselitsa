7fd0dae560b0801f0645415e79501e867cc881bd
chinchilla1337 <vera.shalimova.01@mail.ru>
2025-09-04 18:36:26 +0300

    Delete Паскаль.py

--- Паскаль.py
+++
@@ -1,7 +0,0 @@
-# функция возвращает строку треугольника паскаля с номером n(нумерация с нуля)
-def pascal(n):
-    li = [[1] * i for i in range(1, n + 2)]
-    for j in range(len(li)):
-        for m in range(1, len(li[j]) - 1):
-            li[j][m] = li[j - 1][m - 1] + li[j - 1][m]
-    return li[n]
