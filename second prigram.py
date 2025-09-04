40dbb4a9cf265a4582987254db16d5766273a6df
chinchilla1337 <vera.shalimova.01@gmail.com>
2025-09-04 18:08:55 +0300

    удаляю

--- second prigram.py
+++
@@ -1,6 +0,0 @@
-a = int(input())
-s = input()
-if 10 <= a <= 15 and s == "f":
-    print("YES")
-else:
-    print("NO")
--- сдать степик последн.py
+++
@@ -1,4 +0,0 @@
-from decimal import *
-
-d = Decimal(input())
-print(d.exp() + d.ln() + d.log10() + d.sqrt())
